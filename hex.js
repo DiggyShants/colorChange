bcf562b58b5c1d86a7ce623a64e02a855f4e322b
DiggyShants <dalton.flanagan@gmail.com>
2021-11-20 17:44:13 +0800

    fix

--- hex.js
+++ hex.js
@@ -6,12 +6,12 @@ const color = document.querySelector(".color");
 btn.addEventListener("click", function() {
     let hexColor = "#";
     for(let i = 0; i<6;i++){
-        hexColor += getRandomNumber(); 
+        hexColor += hex[getRandomNumber()]; 
     }
     color.textContent = hexColor;
     document.body.style.backgroundColor = hexColor;
 });
 
 function getRandomNumber() {
-    return hex[Math.floor(Math.random() * hex.length)];
+    return Math.floor(Math.random() * hex.length);
 };
